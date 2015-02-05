dcc7e5bf00f8cc7db6941df588b21b7bdcd46177
Martin Marko <marcus@gratex.com>
2015-02-06 00:00:49 +0100

    very first trivial repl, but resolving promises values
    TODO: arrays of promises and more

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("index");
+var webdriver = require("selenium-webdriver");
+var ff = require('selenium-webdriver/firefox');
+var driver = new ff.Driver();
+
+var By = webdriver.By;
+var until = webdriver.until;
+
+
+
+
+var repl = require("repl").start({
+    prompt: "SELENIUM-REPL>",
+    "eval": function(cmd, context, filename, callback) {
+        webdriver.promise.controlFlow().on('uncaughtException', function(ex) {
+            callback(ex, null);
+        });
+        webdriver.promise.controlFlow().execute(function() {
+            /*jshint evil:true */
+            return eval(cmd);
+        }).then(function(r) {
+            callback(null, r);
+        }).thenCatch(function(ex) {
+            callback(ex, null);
+        });
+    }
+});
+repl.context.webdriver = webdriver;
+repl.context.driver = driver;
+
+repl.on("exit", function() {
+    driver.quit();
+});
